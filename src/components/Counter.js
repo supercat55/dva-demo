90a2a836d793a9d3793efecdb7f683fb33dc85ed
zal <>
2018-12-12 23:19:13 +0800

    subscriptions 订阅

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, routerRedux } from 'dva/router';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, routerRedux } from "dva/router";
 
 const Counter = ({ counter, dispatch }, context) => {
-    console.log(dispatch, 'dispatch');
-    return (
-        <div>
-            <h1>{counter.count}</h1>
-            <button onClick={ () => dispatch(routerRedux.push('/')) }>routerRedux back home</button>
-            <Link to='/'>Home Page</Link>
-            <button onClick={ () => { context.router.history.push('/') } }>Context Go Back</button>
-            <button onClick={ () => { dispatch({ type: 'counter/add' })} }>add</button>
-            <button onClick={ () => { dispatch({ type: 'counter/asyncAdd' })} }>async</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>{counter.count}</h1>
+      <button onClick={() => dispatch(routerRedux.push("/"))}>
+        routerRedux back home
+      </button>
+      <Link to="/">Home Page</Link>
+      <button
+        onClick={() => {
+          context.router.history.push("/");
+        }}
+      >
+        Context Go Back
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "counter/add" });
+        }}
+      >
+        add
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "counter/asyncAdd" });
+        }}
+      >
+        async
+      </button>
+    </div>
+  );
+};
 
 // Counter.propTypes = {
 //     // counter: PropTypes.object
 // }
 Counter.contextTypes = {
-    router: PropTypes.object
-}
+  router: PropTypes.object
+};
 
 export default Counter;
--- src/models/counter.js
+++ src/models/counter.js
@@ -1,32 +1,56 @@
-import { routerRedux } from 'dva/router';
-import queryString from 'query-string';
+import { routerRedux } from "dva/router";
+import queryString from "query-string";
 
 export default {
-    namespace: 'counter',
-    state: {
-        count: 1
-    },
-    reducers: {
-        add(state, action) {
-            console.log(action);
-            return {
-                count: state.count + 1
-            }
-        }
-    },
-    effects: {
-        *asyncAdd({ payload }, { call, put, select }) {
-            // const counter = yield select(state => state.counter);
-            // const counter = yield select(({ counter }) => counter);
-            // const { counter } = yield select(_ => _);
-            // console.log(co);
-            yield put({ type: 'add' });
-            yield put(routerRedux.push({
-                pathname: '/',
-                search: queryString.stringify({
-                    name: 'zal'
-                })
-            }));
-        }
+  namespace: "counter",
+  state: {
+    count: 1
+  },
+  reducers: {
+    add(state, action) {
+      console.log(action);
+      return {
+        count: state.count + 1
+      };
     }
-}
+  },
+  /** 订阅数据源，根据条件dispatch需要的action, 数据源可以是当前的时间，服务器的websocket、keyboard输入、geolocation变化、history路由变化等 */
+  subscriptions: {
+    setup({ dispatch }) {
+      window.onresize = () => {
+        dispatch({ type: "add" });
+      };
+    },
+    documentOnclik({ dispatch }) {
+      document.addEventListener("click", () => {
+        dispatch({ type: "add" });
+      });
+    },
+    setupHistory({ dispatch, history }) {
+      history.listen(location => {
+        console.log(location);
+
+        if (location.pathname === "/counter") {
+          dispatch({ type: "add" });
+        }
+      });
+    }
+  },
+  effects: {
+    *asyncAdd({ payload }, { call, put, select }) {
+      // const counter = yield select(state => state.counter);
+      // const counter = yield select(({ counter }) => counter);
+      // const { counter } = yield select(_ => _);
+      // console.log(co);
+      yield put({ type: "add" });
+      yield put(
+        routerRedux.push({
+          pathname: "/",
+          search: queryString.stringify({
+            name: "zal"
+          })
+        })
+      );
+    }
+  }
+};
