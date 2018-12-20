0c1d2f2c74b2e76afecfa22873c4d68afb46e1a8
zal <>
2018-12-20 14:12:13 +0800

    zal styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,32 @@
 import React, { Component } from "react";
 import { CDNFlagIcon } from "react-flag-kit";
+import styled, { keyframes } from "styled-components";
+
+const AppWrapper = styled.div`
+  background-color: ${props => (props.isBlack ? "#222" : "red")};
+`;
+
+const SeleteWrapper = styled.select`
+  outline: 0;
+  overflow: hidden;
+  height: 30px;
+  color: #555;
+  border: 1px solid #ccc;
+  padding: 5px 3px 5px 10px;
+  border-radius: 10px;
+  width: 250px;
+  font-size: 1rem;
+`;
+
+const TextScale = keyframes`
+  from{transform: scale(1.5); }
+  to{transform: scale(.5); }
+`;
+
+const H1Wrapper = styled.h1`
+  margin: 0 auto;
+  animation: ${TextScale} infinite 5s linear;
+`;
 
 class App extends Component {
   render() {
@@ -7,15 +34,18 @@ class App extends Component {
     const { state, updateLocale } = this.props;
     const { locale, flag, content } = state;
     return (
-      <div className="app">
-        <select value={locale} onChange={e => updateLocale(e.target.value)}>
+      <AppWrapper className="app" isBlack>
+        <SeleteWrapper
+          value={locale}
+          onChange={e => updateLocale(e.target.value)}
+        >
           <option value="en-US">English</option>
           <option value="fr-FR">French</option>
           <option value="es-ES">Spanish</option>
-        </select>
+        </SeleteWrapper>
         <CDNFlagIcon code={flag} size={256} />
-        <h1>{content}</h1>
-      </div>
+        <H1Wrapper>{content}</H1Wrapper>
+      </AppWrapper>
     );
   }
 }
